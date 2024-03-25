c3a87abcfd87523936b821ef044e1366d575e507
Guilherme Braga <guilhermebraga.lemobs@gmail.com>
2024-03-25 19:21:00 -0300

    Alterando icon info pra play e incluindo play do audio no bottonsheet.

--- lib/main.dart
+++ lib/main.dart
@@ -29,23 +29,44 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MyHomePageState extends State<MyHomePage> with ChangeNotifier {
   final List<Animal> animais = [
-    Animal('Cachorro', 'assets/images/cachorrinho.jpg', 'assets/audio/cachorro.mp3', 'O cachorro é um animal doméstico popular, conhecido por sua lealdade e inteligência.'),
-    Animal('Gata', 'assets/images/gatinha.jpg', 'assets/audio/gato.mp3', 'A gata é um animal doméstico independente e brincalhão, conhecido por sua agilidade e graça.'),
-    Animal('Vaca', 'assets/images/vaquinha.jpg', 'assets/audio/vaca.mp3', 'A vaca é um animal de fazenda importante, conhecida por sua produção de leite e carne.'),
-    Animal('Ovelha', 'assets/images/ovelha.jpg', 'audio/ovelha.mp3', 'A ovelha é um animal de fazenda dócil e sociável, conhecida por sua lã macia.'),
-    Animal('Aranha', 'assets/images/aranha.jpg', 'assets/audio/ovelha.mp3', 'A aranha é um aracnídeo com oito patas, muitas vezes temido por sua teia e veneno.'),
-    Animal('Cabrito', 'assets/images/cabrito.jpg', 'assets/audio/ovelha.mp3', 'O cabrito é um animal de fazenda jovem e brincalhão, conhecido por sua energia e vitalidade.'),
-    Animal('Cavalo', 'assets/images/cavalo.jpg', 'assets/audio/ovelha.mp3', 'O cavalo é um animal de grande porte, conhecido por sua força e velocidade.'),
-    Animal('Galinha', 'assets/images/galinha.jpg', 'assets/audio/ovelha.mp3', 'A galinha é uma ave doméstica popular, conhecida por sua produção de ovos.'),
-    Animal('Galo', 'assets/images/galo.jpg', 'assets/audio/ovelha.mp3', 'O galo é uma ave doméstica conhecida por seu canto matinal e por proteger as galinhas.'),
-    Animal('Grilo', 'assets/images/grilo.jpg', 'assets/audio/ovelha.mp3', 'O grilo é um inseto conhecido por seu canto estridente e por seus saltos acrobáticos.'),
-    Animal('Hipopotamo', 'assets/images/hipopotamo.jpg', 'assets/audio/ovelha.mp3', 'O hipopótamo é um animal semi-aquático de grande porte, conhecido por sua natureza territorial e agressiva.'),
-    Animal('Macaco', 'assets/images/macaquinho.jpg', 'assets/audio/ovelha.mp3', 'O macaco é um primata conhecido por sua inteligência e habilidades acrobáticas.'),
-    Animal('Porco', 'assets/images/porquinho.jpg', 'assets/audio/ovelha.mp3', 'O porco é um animal de fazenda conhecido por sua inteligência e sociabilidade.'),
-    Animal('Urso', 'assets/images/ursinho.jpg', 'assets/audio/ovelha.mp3', 'O urso é um animal selvagem de grande porte, conhecido por sua força e hibernação.'),
-    Animal('Zebra', 'assets/images/zebra.jpg', 'assets/audio/ovelha.mp3', 'A zebra é um animal selvagem herbívoro, conhecido por suas listras pretas e brancas.'),
+    Animal(
+        'Cachorro',
+        'assets/images/cachorrinho.jpg',
+        'assets/audio/cachorro.mp3',
+        'O cachorro é um animal doméstico popular, conhecido por sua lealdade e inteligência.'),
+    Animal('Gata', 'assets/images/gatinha.jpg', 'assets/audio/gato.mp3',
+        'A gata é um animal doméstico independente e brincalhão, conhecido por sua agilidade e graça.'),
+    Animal('Vaca', 'assets/images/vaquinha.jpg', 'assets/audio/vaca.mp3',
+        'A vaca é um animal de fazenda importante, conhecida por sua produção de leite e carne.'),
+    Animal('Ovelha', 'assets/images/ovelha.jpg', 'audio/ovelha.mp3',
+        'A ovelha é um animal de fazenda dócil e sociável, conhecida por sua lã macia.'),
+    Animal('Aranha', 'assets/images/aranha.jpg', 'assets/audio/ovelha.mp3',
+        'A aranha é um aracnídeo com oito patas, muitas vezes temido por sua teia e veneno.'),
+    Animal('Cabrito', 'assets/images/cabrito.jpg', 'assets/audio/ovelha.mp3',
+        'O cabrito é um animal de fazenda jovem e brincalhão, conhecido por sua energia e vitalidade.'),
+    Animal('Cavalo', 'assets/images/cavalo.jpg', 'assets/audio/ovelha.mp3',
+        'O cavalo é um animal de grande porte, conhecido por sua força e velocidade.'),
+    Animal('Galinha', 'assets/images/galinha.jpg', 'assets/audio/ovelha.mp3',
+        'A galinha é uma ave doméstica popular, conhecida por sua produção de ovos.'),
+    Animal('Galo', 'assets/images/galo.jpg', 'assets/audio/ovelha.mp3',
+        'O galo é uma ave doméstica conhecida por seu canto matinal e por proteger as galinhas.'),
+    Animal('Grilo', 'assets/images/grilo.jpg', 'assets/audio/ovelha.mp3',
+        'O grilo é um inseto conhecido por seu canto estridente e por seus saltos acrobáticos.'),
+    Animal(
+        'Hipopotamo',
+        'assets/images/hipopotamo.jpg',
+        'assets/audio/ovelha.mp3',
+        'O hipopótamo é um animal semi-aquático de grande porte, conhecido por sua natureza territorial e agressiva.'),
+    Animal('Macaco', 'assets/images/macaquinho.jpg', 'assets/audio/ovelha.mp3',
+        'O macaco é um primata conhecido por sua inteligência e habilidades acrobáticas.'),
+    Animal('Porco', 'assets/images/porquinho.jpg', 'assets/audio/ovelha.mp3',
+        'O porco é um animal de fazenda conhecido por sua inteligência e sociabilidade.'),
+    Animal('Urso', 'assets/images/ursinho.jpg', 'assets/audio/ovelha.mp3',
+        'O urso é um animal selvagem de grande porte, conhecido por sua força e hibernação.'),
+    Animal('Zebra', 'assets/images/zebra.jpg', 'assets/audio/ovelha.mp3',
+        'A zebra é um animal selvagem herbívoro, conhecido por suas listras pretas e brancas.'),
   ];
 
   final AudioPlayer audioPlayer = AudioPlayer();
@@ -88,24 +109,31 @@ class _MyHomePageState extends State<MyHomePage> {
                                 ),
                               ),
                               Positioned(
-                                top: 2,
-                                right: 2,
-                                child: IconButton(
-                                  iconSize: 38,
-                                  icon: Icon(Icons.play_circle_outline_outlined, color: Colors.white,),
-                                  onPressed: () async {
-                                    //await audioPlayer.stop(); // Stop any previously playing sound
-                                    await audioPlayer.play(AssetSource(animais[index].audio));
-                                    musicPlayng = true;
-                                    showAnimalInfo(context, animais[index]);
-                                  },
+                                child: Align(
+                                  alignment: Alignment.center,
+                                  child: Opacity(
+                                    opacity: 0.5,
+                                    child: IconButton(
+                                      iconSize: 86,
+                                      icon: const Icon(
+                                        Icons.play_circle_outline_outlined,
+                                        color: Colors.white,
+                                      ),
+                                      onPressed: () async {
+                                        //await audioPlayer.stop(); // Stop any previously playing sound
+                                        await audioPlayer.play(
+                                            AssetSource(animais[index].audio));
+                                        musicPlayng = true;
+                                        showAnimalInfo(context, animais[index]);
+                                      },
+                                    ),
+                                  ),
                                 ),
                               ),
                             ],
                           ),
                         ),
                       ),
-                      // ... rest of your card content
                       Positioned(
                         bottom: 4,
                         left: 4,
@@ -156,6 +184,9 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   void showAnimalInfo(BuildContext context, Animal animal) {
+    final ValueNotifier<IconData> iconNotifier =
+        ValueNotifier(Icons.play_circle_outline_outlined);
+
     showModalBottomSheet(
       context: context,
       shape: const RoundedRectangleBorder(
@@ -230,19 +261,22 @@ class _MyHomePageState extends State<MyHomePage> {
                     right: 5,
                     child: IconButton(
                       iconSize: 56,
-                      icon: Icon(
-                        musicPlayng ? Icons.pause_circle_outline_outlined : Icons.play_circle_outline_outlined,
-                        color: Colors.white,
-                      ),
+                      icon: ValueListenableBuilder<IconData>(
+                          valueListenable: iconNotifier,
+                          builder: (context, value, child) => Icon(
+                              musicPlayng
+                                  ? Icons.pause_circle_outline_outlined
+                                  : Icons.play_circle_outline_outlined,
+                              color: Colors.white)),
                       onPressed: () async {
                         if (musicPlayng) {
-                          await audioPlayer.pause();
+                          await audioPlayer.stop();
                           musicPlayng = false;
                         } else {
-                          await audioPlayer.resume();
+                          await audioPlayer.play(AssetSource(animal.audio));
                           musicPlayng = true;
                         }
-                        setState(() {}); // Update the icon
+                        setState(() {});
                       },
                     ),
                   ),
@@ -253,6 +287,13 @@ class _MyHomePageState extends State<MyHomePage> {
         );
       },
     );
+    audioPlayer.onPlayerComplete.listen((playerState) {
+      musicPlayng = false;
+      iconNotifier.value = musicPlayng
+          ? Icons.pause_circle_outline_outlined
+          : Icons.play_circle_outline_outlined;
+      iconNotifier.notifyListeners();
+    });
   }
 }
 
@@ -264,4 +305,3 @@ class Animal {
 
   Animal(this.nome, this.imagem, this.audio, this.info);
 }
-
